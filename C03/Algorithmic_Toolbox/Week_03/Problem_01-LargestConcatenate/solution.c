5cfdceef17166e76f2d893d9ca8fa3a364f5bcb6
ArielMAJ <ariel.maj@hotmail.com>
2022-12-26 18:30:41 -0300

    [UPT] [/C03/Week_03] Problem 01 solution updated
    It will now receive numbers separated by spaces instead of everything together. It used to accept only single digit numbers, now it can be any digit size (up to 200 characters).

--- C03/Algorithmic_Toolbox/Week_03/Problem_01-LargestConcatenate/solution.c
+++ C03/Algorithmic_Toolbox/Week_03/Problem_01-LargestConcatenate/solution.c
@@ -5,17 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *get_string_from_stdin();
-char *find_max_position(char *position_in_string);
-char *largest_concatenate(char *numbers);
-void move_following_values_one_position_back_effectively_removing_the_current_character(char *position_in_string);
+char *get_input_from_stdin();
+int *process_input(char *stdin_input);
+int *find_max_position(int *position_in_array);
+char *largest_concatenate(int *numbers_array);
+void move_following_values_one_position_back_effectively_removing_the_current_character(int *position_in_array);
 // void pop_value_from_string(char * position_in_string, char value);
 // char * find_value_position_in_string(char * position_in_string, char value);
 
 int main()
 {
-    char *stdin_input = get_string_from_stdin();
-    char *result = largest_concatenate(stdin_input);
+    char *stdin_input = get_input_from_stdin();
+    int *array = process_input(stdin_input);
+    char *result = largest_concatenate(array);
     printf("%s\n", result);
 
     free(stdin_input);
@@ -23,46 +25,71 @@ int main()
     return 0;
 }
 
-char *get_string_from_stdin()
+char *get_input_from_stdin()
 {
     char *stdin_input = (char *) malloc(200 * sizeof(char));
-    scanf("%s", stdin_input);
+    fgets(stdin_input, 200, stdin);
 
     return stdin_input;
 }
 
-char *largest_concatenate(char *numbers)
+int *process_input(char *stdin_input)
 {
-    int str_len = strlen(numbers);
-    char *result = (char *) malloc((str_len + 1) * sizeof(char));
+    char *current_number = stdin_input;
+    char *end_of_number = stdin_input;
+    int *array = (int *) malloc(200 * sizeof(int));
     int pos = 0;
-    while (str_len-- != 0)
+    while (*end_of_number != '\0')
     {
-        char *max_position = find_max_position(numbers);
-        result[pos++] = *max_position;
+        if (*end_of_number == ' ')
+        {
+            *end_of_number = '\0';
+            array[pos++] = atoi(current_number);
+            current_number = end_of_number + 1;
+        }
+        end_of_number++;
+    }
+    array[pos++] = atoi(current_number);
+    array[pos] = -1;
+
+    return array;
+}
+
+char *largest_concatenate(int *numbers_array)
+{
+    char *result = (char *) malloc(200 * sizeof(char));
+    result[0] = '\0';
+
+    int pos = 0;
+    while (numbers_array[pos] != -1)
+    {
+        int *max_position = find_max_position(numbers_array);
+        char buffer[100];
+        buffer[0] = '\0';
+        sprintf(buffer, "%d", *max_position);
+        strcat(result, buffer);
         move_following_values_one_position_back_effectively_removing_the_current_character(max_position);
     }
-    result[pos] = '\0';
 
     return result;
 }
 
-void move_following_values_one_position_back_effectively_removing_the_current_character(char *position_in_string)
+void move_following_values_one_position_back_effectively_removing_the_current_character(int *position_in_array)
 {
-    while (*position_in_string != '\0')
+    while (*position_in_array != -1)
     {
-        *position_in_string = *(position_in_string + 1);
-        position_in_string++;
+        *position_in_array = *(position_in_array + 1);
+        position_in_array++;
     }
 }
 
-char *find_max_position(char *position_in_string)
+int *find_max_position(int *position_in_array)
 {
-    char *max_position = position_in_string;
-    while (*(++position_in_string) != '\0')
+    int *max_position = position_in_array;
+    while (*(++position_in_array) != -1)
     {
-        if (*position_in_string > *max_position)
-            max_position = position_in_string;
+        if (*position_in_array > *max_position)
+            max_position = position_in_array;
     }
     return max_position;
 }
