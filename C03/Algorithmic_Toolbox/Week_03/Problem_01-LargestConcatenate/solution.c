cac7b525de50be96042d7003fa0f7a1346e75996
ArielMAJ <ariel.maj@hotmail.com>
2022-12-26 18:45:49 -0300

    [UPT] [/C03/Week_03] Problem 01 solution updated
    Updated it to work with characters only again (instead of comparing ints) but receiving the new input (separated by spaces).

--- C03/Algorithmic_Toolbox/Week_03/Problem_01-LargestConcatenate/solution.c
+++ C03/Algorithmic_Toolbox/Week_03/Problem_01-LargestConcatenate/solution.c
@@ -1,23 +1,18 @@
-// This solution is O(n^2) as it uses the "find_max" function (which is O(n)) n times.
-// It could be O(nlogn) by using an efficient (reverse) sorting algorithm.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *get_input_from_stdin();
-int *process_input(char *stdin_input);
-int *find_max_position(int *position_in_array);
-char *largest_concatenate(int *numbers_array);
-void move_following_values_one_position_back_effectively_removing_the_current_character(int *position_in_array);
-// void pop_value_from_string(char * position_in_string, char value);
-// char * find_value_position_in_string(char * position_in_string, char value);
+char *get_string_from_stdin();
+void process_input(char *current_character);
+char *find_max_position(char *position_in_string);
+char *largest_concatenate(char *numbers);
+void move_following_values_one_position_back_effectively_removing_the_current_character(char *position_in_string);
 
 int main()
 {
-    char *stdin_input = get_input_from_stdin();
-    int *array = process_input(stdin_input);
-    char *result = largest_concatenate(array);
+    char *stdin_input = get_string_from_stdin();
+    process_input(stdin_input);
+    char *result = largest_concatenate(stdin_input);
     printf("%s\n", result);
 
     free(stdin_input);
@@ -25,7 +20,7 @@ int main()
     return 0;
 }
 
-char *get_input_from_stdin()
+char *get_string_from_stdin()
 {
     char *stdin_input = (char *) malloc(200 * sizeof(char));
     fgets(stdin_input, 200, stdin);
@@ -33,76 +28,48 @@ char *get_input_from_stdin()
     return stdin_input;
 }
 
-int *process_input(char *stdin_input)
+void process_input(char *current_character)
 {
-    char *current_number = stdin_input;
-    char *end_of_number = stdin_input;
-    int *array = (int *) malloc(200 * sizeof(int));
-    int pos = 0;
-    while (*end_of_number != '\0')
+    while (*current_character != '\0')
     {
-        if (*end_of_number == ' ')
-        {
-            *end_of_number = '\0';
-            array[pos++] = atoi(current_number);
-            current_number = end_of_number + 1;
-        }
-        end_of_number++;
+        if (*current_character == ' ')
+            move_following_values_one_position_back_effectively_removing_the_current_character(current_character);
+        current_character++;
     }
-    array[pos++] = atoi(current_number);
-    array[pos] = -1;
-
-    return array;
 }
 
-char *largest_concatenate(int *numbers_array)
+char *largest_concatenate(char *numbers)
 {
-    char *result = (char *) malloc(200 * sizeof(char));
-    result[0] = '\0';
-
+    int str_len = strlen(numbers);
+    char *result = (char *) malloc((str_len + 1) * sizeof(char));
     int pos = 0;
-    while (numbers_array[pos] != -1)
+    while (str_len-- != 0)
     {
-        int *max_position = find_max_position(numbers_array);
-        char buffer[100];
-        buffer[0] = '\0';
-        sprintf(buffer, "%d", *max_position);
-        strcat(result, buffer);
+        char *max_position = find_max_position(numbers);
+        result[pos++] = *max_position;
         move_following_values_one_position_back_effectively_removing_the_current_character(max_position);
     }
+    result[pos] = '\0';
 
     return result;
 }
 
-void move_following_values_one_position_back_effectively_removing_the_current_character(int *position_in_array)
+void move_following_values_one_position_back_effectively_removing_the_current_character(char *position_in_string)
 {
-    while (*position_in_array != -1)
+    while (*position_in_string != '\0')
     {
-        *position_in_array = *(position_in_array + 1);
-        position_in_array++;
+        *position_in_string = *(position_in_string + 1);
+        position_in_string++;
     }
 }
 
-int *find_max_position(int *position_in_array)
+char *find_max_position(char *position_in_string)
 {
-    int *max_position = position_in_array;
-    while (*(++position_in_array) != -1)
+    char *max_position = position_in_string;
+    while (*(++position_in_string) != '\0')
     {
-        if (*position_in_array > *max_position)
-            max_position = position_in_array;
+        if (*position_in_string > *max_position)
+            max_position = position_in_string;
     }
     return max_position;
 }
-
-// void pop_value_from_string(char * position_in_string, char value)
-// {
-//     position_in_string = find_value_position_in_string(position_in_string, value);
-//     move_following_values_one_position_back_effectively_removing_the_current_character(position_in_string);
-// }
-
-// char * find_value_position_in_string(char * position_in_string, char value)
-// {
-//     while (*position_in_string != value)
-//         position_in_string++;
-//     return position_in_string;
-// }
