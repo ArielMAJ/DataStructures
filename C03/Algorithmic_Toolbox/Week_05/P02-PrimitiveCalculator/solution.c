b12125e0feb900d130c1d87cc281a9bfb9877dc2
ArielMAJ <ariel.maj@hotmail.com>
2023-01-30 17:08:34 -0300

    [UPT] [C03 AT W5] Updated Primitive Calculator solution.
    This solution uses Dynamic Programming (as expected for Week 05 assignments).

--- C03/Algorithmic_Toolbox/Week_05/P02-PrimitiveCalculator/solution.c
+++ C03/Algorithmic_Toolbox/Week_05/P02-PrimitiveCalculator/solution.c
@@ -8,38 +8,78 @@ typedef struct Vector
     int length;
     int capacity;
     void (*append)(struct Vector *, int);
-    void (*print)(struct Vector *);
+    void (*print_reversed)(struct Vector *);
 } Vector;
 
 int get_int();
 int *create_heap_int_array(int capacity);
 
-Vector *new_vector();
+Vector *new_vector(int capacity);
 void increase_vector_capacity(Vector *this);
 void append_to_vector(Vector *this, int value);
-void print_vector(Vector *this);
+void print_reversed_vector(Vector *this);
 void free_vector(Vector *vect);
 
-// Vector *primitive_calculator(int value);
+int minimal_steps_to_this_position(Vector *dynamic_programming_vect, int i);
+int shortest_path_previous_position(Vector *vect, int number);
+Vector *primitive_calculator(int value);
 
 int main(void)
 {
     int input_integer = get_int();
 
-    Vector *vect = new_vector();
+    Vector *vect = primitive_calculator(input_integer);
 
-    for (int i = 0; i < input_integer; ++i)
-        vect->append(vect, i);
-    vect->print(vect);
+    printf("%d\n", vect->length - 1);
+    vect->print_reversed(vect);
 
     free_vector(vect);
     return 0;
 }
 
-// Vector *primitive_calculator(int value)
-// {
+Vector *primitive_calculator(int value)
+{
+    Vector *dynamic_programming_vect = new_vector(value + 2);
 
-// }
+    dynamic_programming_vect->append(dynamic_programming_vect, 0);
+    for (int i = 1; i < value; ++i)
+        dynamic_programming_vect->append(
+            dynamic_programming_vect,
+            minimal_steps_to_this_position(dynamic_programming_vect, i)
+        );
+
+    Vector *output_vect = new_vector(dynamic_programming_vect->elements[value] + 2);
+
+    while (value >= 1)
+    {
+        output_vect->append(output_vect, value);
+        value = shortest_path_previous_position(dynamic_programming_vect, value);
+    }
+
+    free_vector(dynamic_programming_vect);
+    return output_vect;
+}
+
+int minimal_steps_to_this_position(Vector * dynamic_programming_vect, int i)
+{
+    int previous_position = shortest_path_previous_position(dynamic_programming_vect, i);
+    int minimal_steps = dynamic_programming_vect->elements[previous_position] + 1;
+
+    return minimal_steps;
+}
+
+int shortest_path_previous_position(Vector *vect, int number)
+{
+    if ( number % 3 == 0 && vect->elements[number / 3] <= vect->elements[number / 2])
+        if (vect->elements[number / 3] < vect->elements[number - 1])
+            return number / 3;
+        else
+            return number - 1;
+    else if ( number % 2 == 0 && vect->elements[number / 2] <= vect->elements[number - 1])
+        return number / 2;
+    else
+        return number - 1;
+}
 
 int get_int()
 {
@@ -49,17 +89,15 @@ int get_int()
     return value;
 }
 
-Vector *new_vector()
+Vector *new_vector(int capacity)
 {
-    int capacity = 50;
-
     Vector *vect = (Vector *) malloc(sizeof(Vector));
 
     vect->elements = create_heap_int_array(capacity);
     vect->capacity = capacity;
     vect->length = 0;
     vect->append = append_to_vector;
-    vect->print = print_vector;
+    vect->print_reversed = print_reversed_vector;
 
     return vect;
 }
@@ -79,15 +117,15 @@ int *create_heap_int_array(int capacity)
 
 void increase_vector_capacity(Vector *this)
 {
-    this->capacity *= 4;
+    this->capacity *= 2;
     this->elements = (int *) realloc(this->elements, this->capacity * sizeof(int));
 }
 
-void print_vector(Vector *this)
+void print_reversed_vector(Vector *this)
 {
-    for (int i = 0; i < this->length; ++i)
+    for (int i = this->length - 1; i > 0 ; --i)
         printf("%d ", this->elements[i]);
-    printf("\n");
+    printf("%d\n", this->elements[0]);
 }
 
 void free_vector(Vector *vect)
